015e04f4ab8dd69e00443bfde5c9105d09203140
reihannnh <heriantono.reihan@gmail.com>
2021-01-04 21:15:41 +0700

    MVC

---
+++ 005 - MVC/src/cobamvc/ViewPraktikum.java
@@ -0,0 +1,116 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cobamvc;
+
+import java.awt.ScrollPane;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.JTextField;
+import javax.swing.table.DefaultTableModel;
+
+/**
+ *
+ * @author Orenji
+ */
+public class ViewPraktikum extends JFrame{
+    
+    
+    JLabel jlnim = new JLabel("Nim : ");
+    JTextField jtnim = new JTextField();
+    JLabel jlnama = new JLabel("Nama : ");
+    JTextField jtnama = new JTextField();
+    JLabel jlalamat = new JLabel("Alamat : ");
+    JTextField jtalamat = new JTextField();
+    
+    JTextField jtsearch = new JTextField();
+    JLabel jlsearch = new JLabel("Cari :");
+    
+    JButton jbtambah = new JButton("Tambah");
+    JButton jbupdate = new JButton("Update");
+    JButton jbcancel = new JButton("Cancel");
+    JButton jbsearch = new JButton("Cari NIM");
+    
+    JTable tabel;
+    DefaultTableModel dtm;
+    JScrollPane scrollPane;
+    Object namaKolom[] = {"Nim", "Nama", "Alamat"};
+    
+    public ViewPraktikum(){
+        dtm = new DefaultTableModel(namaKolom, 0);
+        tabel = new JTable(dtm);
+        scrollPane = new JScrollPane(tabel);
+        
+        setTitle("Data Mahasiswa");
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setVisible(true);
+        setLayout(null);
+        setSize(750,400); // x , y
+        
+        // CARI
+        // setBounds(m,n,o,p)
+	// m = posisi x; n = posisi y
+	// o = panjang komponen; p = tinggi komponen
+         
+        add(jtsearch);
+        jtsearch.setBounds(20,30, 350, 20);
+        add(jbsearch);
+        jbsearch.setBounds(400,30,100,20);
+        
+        add(scrollPane);
+        scrollPane.setBounds(20, 70, 480, 200);
+        
+        // NIM =================================================================
+        add(jlnim);
+        jlnim.setBounds(510, 20, 90, 20);
+        add(jtnim);
+        jtnim.setBounds(510, 40, 120, 20);
+        
+        // NAMA ================================================================
+        add(jlnama);
+        jlnama.setBounds(510, 60, 90, 20);
+        add(jtnama);
+        jtnama.setBounds(510, 80, 120, 20);
+        
+        // ALAMAT ==============================================================
+        add(jlalamat);
+        jlalamat.setBounds(510, 100, 90, 20);
+        add(jtalamat);
+        jtalamat.setBounds(510, 120, 120, 20);
+        
+        
+        // BUTTON ==============================================================
+        add(jbtambah);
+        jbtambah.setBounds(510, 160, 90, 20);
+        
+        add(jbupdate);
+        jbupdate.setBounds(510, 200, 90, 20);
+        
+        add(jbcancel);
+        jbcancel.setBounds(510, 240, 90, 20);
+    }
+     
+    
+    //Buat ngambil data texfield
+    public String getNim(){
+        return jtnim.getText();
+    }
+    
+    public String getNama(){
+        return jtnama.getText();
+    }
+    
+    public String getAlamat(){
+        return jtalamat.getText();
+    }
+    
+    public String getSearch(){
+        return jtsearch.getText();
+    }
+}
---
+++ praktikum.sql
@@ -0,0 +1,58 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 11, 2020 at 03:29 PM
+-- Server version: 10.4.11-MariaDB
+-- PHP Version: 7.4.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `praktikum`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `mahasiswa`
+--
+
+CREATE TABLE `mahasiswa` (
+  `nama` varchar(20) NOT NULL,
+  `nim` char(9) NOT NULL,
+  `alamat` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `mahasiswa`
+--
+
+INSERT INTO `mahasiswa` (`nama`, `nim`, `alamat`) VALUES
+('liek', '123180054', 'seturan'),
+('anu', '123180055', 'sana');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `mahasiswa`
+--
+ALTER TABLE `mahasiswa`
+  ADD PRIMARY KEY (`nim`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
